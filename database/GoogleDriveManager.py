df8b27f5cb86cc80adbb24cac3443c95e8e096ba
Maciej Praczuk <voluxus@protonmail.com>
2024-06-18 23:28:30 +0200

    GDrive auth troubleshooting

--- database/GoogleDriveManager.py
+++ database/GoogleDriveManager.py
@@ -5,19 +5,19 @@ from pydrive.drive import GoogleDrive
 
 
 class GoogleDriveManager:
-    def __init__(self, settings_file='drive_settings.yaml', folder_name='EMG Analysis Data'):
+    def __init__(self, settings_file='drive_settings.yaml', app_folder_name='EMG Analysis Data'):
         self.settings_file = settings_file
-        self.folder_name = folder_name
+        self.app_folder_name = app_folder_name
 
         self.gauth = GoogleAuth(settings_file=self.settings_file)
         self.authenticate_persistently()
         self.drive = GoogleDrive(self.gauth)
 
-        self.folder_id = self.ensure_app_folder()
+        self.app_folder_id = self.ensure_app_folder()
 
     def authenticate_persistently(self):
         credentials_file = self.gauth.settings.get('save_credentials_file')
-        assert credentials_file, 'Credentials file path not set in settings file'
+        assert credentials_file, 'Credentials file path not set in the settings file'
 
         if os.path.exists(credentials_file):
             self.gauth.LoadCredentialsFile(credentials_file)
@@ -32,15 +32,18 @@ class GoogleDriveManager:
         self.gauth.SaveCredentialsFile(credentials_file)
 
     def ensure_app_folder(self):
-        folder_id = self._get_folder_id()
+        folder_id = self.get_folder_id()
 
         if folder_id is None:
-            folder_id = self._create_folder()
+            folder_id = self.create_folder()
 
         return folder_id
 
-    def _get_folder_id(self):
-        query = f"title='{self.folder_name}' and mimeType='application/vnd.google-apps.folder' and trashed=false"
+    def get_folder_id(self, folder_path=None, parent_folder_id='root'):  # TODO nested folders
+        if not folder_path:
+            folder_path = self.app_folder_name
+
+        query = f"title='{self.app_folder_name}' and mimeType='application/vnd.google-apps.folder' and trashed=false"
         file_list = self.drive.ListFile({'q': query}).GetList()
         assert not len(file_list) > 1, 'Multiple folders with the same name found'
 
@@ -49,10 +52,11 @@ class GoogleDriveManager:
             return folder_id
         return None
 
-    def _create_folder(self):
+    def create_folder(self, parents_folder_id='root'):
         folder_metadata = {
-            'title': self.folder_name,
-            'mimeType': 'application/vnd.google-apps.folder'
+            'title': self.app_folder_name,
+            'mimeType': 'application/vnd.google-apps.folder',
+            'parents': [{'id': parents_folder_id}]
         }
         folder = self.drive.CreateFile(folder_metadata)
         folder.Upload()
--- database/main.py
+++ database/main.py
@@ -1,21 +1,10 @@
-import sys
-
-from PyQt6.QtWidgets import QApplication
-
 from database.GoogleDriveManager import GoogleDriveManager
-from database.MockUI import MockUI
 
 
 def main():
-    app = QApplication(sys.argv)
-    window = MockUI()
-
     drive_manager = GoogleDriveManager()
-    drive_manager.upload_file('local_file_example.txt', 'remote_file.txt', drive_manager.folder_id)
-    drive_manager.download_file(drive_manager.folder_id, 'remote_file.txt', 'downloaded_file.txt')
-
-    window.show()
-    sys.exit(app.exec())
+    drive_manager.upload_file('local_file_example.txt', 'remote_file.txt', drive_manager.app_folder_id)
+    drive_manager.download_file(drive_manager.app_folder_id, 'remote_file.txt', 'downloaded_file.txt')
 
 
 if __name__ == '__main__':
